ef5faf698123cfb949f521c409fa2097d80ff924
Tori Hedden <victoria.hedden@bcbsnc.com>
2018-02-08 11:40:37 -0500

    add demo play button

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
-var statusData = [percentage];
-var percentage = spoofStatus(0);
+document.getElementById('status-bar-button').addEventListener('click', runStatusBarDemo);
+
 var statusWidth = window.innerWidth - 15;
 var statusHeight = 50;
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function spoofStatus (n) {
-  setTimeout(function() {
-    if (n < 100) {
-      // adding randomization to look even more like a loading status bar
-      n += getRandomInt(20);
-      createStatusBar(n);
-      spoofStatus(n);
-    }
-  }, 1000);
-}
-
 var statusSvg = d3.select('body')
   .append('svg')
   .attr('width', statusWidth)
   .attr('height', statusHeight)
   .classed('status-bar', true)
-  .style('background-color', 'gray')
+  .style('background-color', 'gray');
 
-function createStatusBar (percentage) {
+function runStatusBarDemo () {
 
-  // This line here is the secret sauce. You must destroy the old SVG and create a new one in order to see the updated status
-  d3.select('.current-status').remove();
+  var disableButton = disableDemoButton();
+  var statusData = [percentage];
+  var percentage = spoofStatus(0);
 
-  statusSvg.selectAll('rect')
-    .data(statusData)
-    .enter()
-    .append('rect')
-    .classed('current-status', true)
-    .attr('fill', 'lightgreen')
-    .attr('height', statusHeight)
-    .attr('width', ((percentage /  100) * statusWidth))
+  function disableDemoButton () {
+    document.getElementById('status-bar-button').setAttribute('disabled', true);
+  }
+
+  function reenableDemoButton () {
+    document.getElementById('status-bar-button').removeAttribute('disabled');
+  }
+
+  function spoofStatus (n) {
+    setTimeout(function() {
+      if (n < 100) {
+        n += getRandomInt(20);
+        createStatusBar(n);
+        spoofStatus(n);
+      } else {
+        reenableDemoButton();
+      }
+    }, 1000);
+  }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  function createStatusBar (percentage) {
+    d3.select('.current-status').remove();
+    statusSvg.selectAll('rect')
+      .data(statusData)
+      .enter()
+      .append('rect')
+      .classed('current-status', true)
+      .attr('fill', 'lightgreen')
+      .attr('height', statusHeight)
+      .attr('width', ((percentage /  100) * statusWidth));
+  }
 }
