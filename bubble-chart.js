0a4d9622f924f56d5f6247f3bf7a29d865055617
Tori Hedden <victoria.hedden@bcbsnc.com>
2018-02-28 10:26:35 -0500

    just a push

--- bubble-chart.js
+++ bubble-chart.js
@@ -14,8 +14,8 @@ d3.csv('data/medium_january.csv', function(error, data) {
 var width = 960,
     height = 960,
     maxRadius = 6,
-    columnForColors = "category",
-    columnForRadius = "views";
+    columnForColors = 'category',
+    columnForRadius = 'views';
 
 function createBubbleChart (data) {
   // d3.select('#bubble-chart')
@@ -34,16 +34,20 @@ function createBubbleChart (data) {
       .attr('height', 500)
       .attr('style', 'border: solid 1px red;')
 
-      var circle = svg.selectAll("circle")
+      var circle = svg.selectAll('circle')
                       .data(bubbleData, function(d, i) { return d; });
 
-      circle.enter().append("circle")
-                    .attr("cy", function (d, i) { return 250 - (i * 20); })
-                    .attr("cx", function (d, i) { return 250 - (i * 20); })
-                    .attr("r", function(d, i) { return d.views / 100; })
+      circle.enter().append('circle')
+                    .attr('cy', function (d, i) { return 150 + (i * 25); })
+                    .attr('cx', function (d, i) { return 150 + (i * 25); })
+                    .attr('r', function(d, i) { return d.views / 100; })
                     .attr('text', function(d, i) {
-                      console.log(i);
+                      // console.log(i);
                       return d.title;
+                    })
+                    .attr('fill', function(d, i) {
+                      console.log(d[i]);
+                      return 'rgb(0, 0, ' + (i * 20) + ')';
                     });
 }
 // ----
@@ -58,13 +62,13 @@ function createBubbleChart (data) {
 //             .attr('height', 200)
 //             .attr('style', 'border: solid 1px red;')
 
-// var circle = svg.selectAll("circle")
+// var circle = svg.selectAll('circle')
 //                 .data(circleData, function(d) { return d; });
 
-// circle.enter().append("circle")
-//               .attr("cy", 50)
-//               .attr("cx", 25)
-//               .attr("r", 25);
+// circle.enter().append('circle')
+//               .attr('cy', 50)
+//               .attr('cx', 25)
+//               .attr('r', 25);
 
-              // .attr("cx", function(d, i) { return d; })
-              // .attr("r", function(d) { return d / 5; });
+              // .attr('cx', function(d, i) { return d; })
+              // .attr('r', function(d) { return d / 5; });
