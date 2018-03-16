8bc6e0dd7d207329d04dedb023e667c6db9f0721
Tori Hedden <victoria.hedden@bcbsnc.com>
2018-03-16 16:00:56 -0400

    error handling for bubble chart get data

--- bubble-chart-v4.js
+++ bubble-chart-v4.js
@@ -9,35 +9,40 @@ var svg = d3.select("#bubble-chart").append("svg")
     .attr("height", size);
 
 d3.csv("data/medium_january.csv", type, (error, data) => {
-  var posts = data;
+  if (error) {
+    console.error('Error getting or parsing the data.');
+    throw error;
+  } else {
+    var posts = data;
 
-  var root = d3.hierarchy({children: [{children: posts}]})
-      .sum(d => d.radius * d.radius)
-      .sort(function(a, b) {
-        function comparator(a, b) {
-          return b.value - a.value;
-        }
-      });
+    var root = d3.hierarchy({children: [{children: posts}]})
+        .sum(d => d.radius * d.radius)
+        .sort(function(a, b) {
+          function comparator(a, b) {
+            return b.value - a.value;
+          }
+        });
 
-  pack(root);
+    pack(root);
 
-  svg.selectAll("circle")
-    .data(root.descendants().slice(1))
-    .enter().append("circle")
-      .attr("r", d => { return d.r; })
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y)
-      .attr("class", d => {
-        return d.data.category ? d.data.category.toLowerCase().replace(/ /g, '-') : '';
-      })
-      .filter(d => d.data)
-      .style("fill", d => {
-        return !d.data.category ? gray : '';
-      })
-      .append("title")
-      .text(d => {
-        return !d.data.title && !d.data.views ? '' : `"${d.data.title}"\nviews: ${d.data.views}`;
-      });
+    svg.selectAll("circle")
+      .data(root.descendants().slice(1))
+      .enter().append("circle")
+        .attr("r", d => { return d.r; })
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
+        .attr("class", d => {
+          return d.data.category ? d.data.category.toLowerCase().replace(/ /g, '-') : '';
+        })
+        .filter(d => d.data)
+        .style("fill", d => {
+          return !d.data.category ? gray : '';
+        })
+        .append("title")
+        .text(d => {
+          return !d.data.title && !d.data.views ? '' : `"${d.data.title}"\nviews: ${d.data.views}`;
+        });
+  }
 });
 
 function type(d) {
